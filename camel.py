d54bda57d193a161b27a48785a5bf7f4bddefd04
Ian-503 <ian.sache20@gmail.com>
2026-05-20 12:44:39 -0300

    Create camel.py

---
+++ camel.py
@@ -0,0 +1,10 @@
+camel = input("camelCase: ")
+snake = ""
+
+for i in camel:
+    if i.isupper():
+        snake = snake + "_" + i.lower()
+    else:
+        snake = snake + i
+
+print("snake_case:", snake)
