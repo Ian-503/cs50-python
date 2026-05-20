cb235445167a616e4a4794c33cbdb3208ad06861
Ian-503 <ian.sache20@gmail.com>
2026-05-20 12:49:29 -0300

    Create grocery.py

---
+++ grocery.py
@@ -0,0 +1,15 @@
+lista={}
+while True:
+    try:
+        item = input().upper()
+        if item in lista:
+            lista[item]+=1
+        else:
+            lista[item]=1
+
+
+    except EOFError:
+        for i in sorted(lista.keys()):
+            print(f"{lista[i]} {i}")
+        print()
+        break
